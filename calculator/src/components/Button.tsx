9a852a9a86a718a186acfcbd4df4d1320def324a
SoftBaguette <charles.baroud@icloud.com>
2024-08-06 17:39:45 +0300

    Improvements: + Basic Button + Grid Layout

---
+++ calculator/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+
+`;
+
+const Button: React.FC = () => {
+    return (
+        <StyledButton>Uno</StyledButton>
+    );
+}
+
+export default Button;
--- calculator/src/components/Calculator.tsx
+++ calculator/src/components/Calculator.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "./Button";
 
 const Container = styled.div`
     Background: #323232;
     flex: 1;
 `;
 
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 80px);
+    grid-template-rows: repeat(5, 80px);
+`;
+
 const Calculator: React.FC<{}> = () => {
     return (
-        <Container>Calculator bitches</Container>
+        <Container>
+            <Grid>
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+                <Button />
+            </Grid>
+        </Container>
     );
 }
 
